8a245415ad348acccb0a64dfa3cbc5c83aa0c1a4
Martijn van Exel <m@rtijn.org>
2016-11-29 17:50:27 -0700

    adding flattening, some ui improvements

--- rrr.py
+++ rrr.py
@@ -21,11 +21,20 @@ def get_relations(state_code):
     payload = {'data': '[out:json];relation[network="US:{}"];out meta;'.format(state_code.upper())}
     response = requests.get(overpass_api_url, params=payload)
     relations = response.json()
-    print(relations)
+    out = []
     print(response.url)
     if 'elements' in relations and len(relations['elements']) > 0:
-        return jsonify(relations['elements'])
-    return '{}'
+        print(relations['elements'][0])
+        for element in relations['elements']:
+            del element['members']
+            if 'tags' in element:
+                for tag in element['tags']:
+                    print(tag)
+                    element[tag] = element['tags'][tag]
+            del element['tags']
+            out.append(element)
+        return jsonify(out)
+    return []
 
 if __name__ == "__main__":
     app.run()
