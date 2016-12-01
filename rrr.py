6783001b6e94fc1290216af79c59eae571d4cd41
Martijn van Exel <m@rtijn.org>
2016-11-30 17:45:21 -0700

    better date handling, add links

--- rrr.py
+++ rrr.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, jsonify
 import requests
 import us
 import json
+import iso8601
 
 app = Flask(__name__)
 
@@ -24,15 +25,27 @@ def get_relations(state_code):
     out = []
     if 'elements' in relations and len(relations['elements']) > 0:
         for element in relations['elements']:
-            # flatten the tags and remove members
+            # print(element)
+            # remove members we don't need em
             del element['members']
+            # flatten tags
             if 'tags' in element:
                 for tag in element['tags']:
                     element[tag] = element['tags'][tag]
+            # delete original tags
             del element['tags']
+            if 'user' in element:
+                element['user'] = '<a href="https://osm.org/user/{user}">{user}</a>'.format(user=element['user'])
+            if 'changeset' and 'timestamp' in element:
+                dt = iso8601.parse_date(element['timestamp'])
+                datestring = dt.strftime('%Y-%m-%d %I:%M%p')
+                element['timestamp'] = '<a href="https://www.openstreetmap.org/changeset/{changeset}">{datestring}</a>'.format(
+                    changeset=element['changeset'],
+                    datestring=datestring)
+                #element['timestamp'] = '{datestring}'.format(datestring=datestring)
             out.append(element)
         return jsonify(out)
-    return []
+    return jsonify([])
 
 if __name__ == "__main__":
     app.run()
