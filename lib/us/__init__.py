9044f9adca9cbe1a9b80b1c749de903b98126e7c
Martijn van Exel <m@rtijn.org>
2016-12-13 11:45:29 -0700

    adding gitignore

---
+++ lib/us/__init__.py
@@ -0,0 +1,9 @@
+from . import states
+from .states import (
+    STATES, STATES_CONTIGUOUS, STATES_CONTINENTAL,
+    TERRITORIES, STATES_AND_TERRITORIES, OBSOLETE
+)
+from .unitedstatesofamerica import *
+
+__appname__ = 'us'
+__version__ = '0.9.1'
